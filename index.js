34750f562b43656da1546bf7d0bcbf941dfd4b14
Philippe Gobeil-Bouchard <pgobeil-@student.42.us.org>
2020-10-15 00:10:31 -0400

    added depende ncies

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const Discord = require('discord.js');
 const {prefix, token } = require('./config.json');
 
-const client = new Discord.client();
+const client = new Discord.Client();
 
 client.once('ready', () => {
 	console.log('Ready!');

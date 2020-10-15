115027e4152dd28adf8b7eb55eca3a6beca4b0c3
Philippe Gobeil-Bouchard <pgobeil-@student.42.us.org>
2020-10-14 23:47:10 -0400

    first basic test

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const Discord = require('discord.js');
+const {prefix, token } = require('./config.json');
+
+const client = new Discord.client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	} else if (command === 'beep') {
+		message.channel.send('Boop.');
+	}
+	// other commands...
+});
+
+client.login(token);

7e7bca7fee35d9c6d2f844ec27f3f69a248cb9a3
Philippe Gobeil-Bouchard <pgobeil-@student.42.us.org>
2020-10-15 23:57:44 -0400

    proper reload

--- commands/reload.js
+++ commands/reload.js
@@ -1,14 +1,25 @@
 module.exports = {
 	name: 'reload',
-	description: 'Reloads a command',
+	description: 'Reloads la commande',
 	args: true,
 	execute(message, args) {
-		if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
 		const commandName = args[0].toLowerCase();
 		const command = message.client.commands.get(commandName)
-	|| message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+			|| message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-if (!command) return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
+		if (!command) {
+			return message.channel.send(`C'est pas une commande ça \`${commandName}\`, ${message.author}!`);
+		}
 
+		delete require.cache[require.resolve(`./${command.name}.js`)];
+
+		try {
+			const newCommand = require(`./${command.name}.js`);
+			message.client.commands.set(newCommand.name, newCommand);
+			message.channel.send(`Commande \`${command.name}\` est reloaded!`);
+		} catch (error) {
+			console.error(error);
+			message.channel.send(`je peux pas reload \`${command.name}\`:\n\`${error.message}\``);
+		}
 	},
 };
